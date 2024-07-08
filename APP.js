e220b9aa4155829e7d84fb6e6c02f6da35c4e2a0
tunir24 <140588445+tunir24@users.noreply.github.com>
2024-07-08 23:04:04 +0530

    APP.js

---
+++ APP.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './Form';
+import Header from './Header';
+import Show from './Show';
+
+function App() {
+  return (
+ <>
+ <Header/>
+ <Form/>
+<Show/>
+ </>
+  );
+}
+
+export default App;
